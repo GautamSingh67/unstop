85b748c712c3ca8eac49216008963659d2ddb44e
GautamSingh <lossangle20@gmail.com>
2023-07-20 16:05:54 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useRef, useState } from 'react';
 import './App.css';
 import { Main } from './component/main/Main';
 import { Navbar } from './component/navbar/Navbar';
 
 function App() {
+  const [bool, setBool] = useState(true);
+  const navref = useRef();
+  function handle() {
+    if (bool) {
+      navref.current.style.display = "block";
+      setBool(false);
+    }
+    else {
+      navref.current.style.display = "none";
+      setBool(true);
+    }
+  }
   return (
     <div className="app">
-     <Navbar/>
-     <Main/>
+      <Navbar navref={navref} onclick = {handle}/>
+      <Main onclick={handle} />
     </div>
   );
 }
--- src/component/main/Main.jsx
+++ src/component/main/Main.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import style from './style.module.css'
 import { Card } from './card/Card'
 import { Model } from './model/Model'
-export function Main() {
+export function Main({onclick}) {
+    const cardRef = useRef();
+    const[bool,setBool] = useState(true);
+    function handleOverview(){
+        if(bool){
+        cardRef.current.style.display = "block";
+        setBool(!bool);
+        }
+        else{
+            cardRef.current.style.display = "none";
+            setBool(!bool);
+        }
+    }
+
     return (
         <div className={style['main']}>
             <header className={style['header']}>
-                <svg className={style['svgres']} style={{marginTop:'0.2rem', marginLeft:'0.3rem'}} xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 30 30" fill="none">
+                <svg onClick={onclick} className={style['svgres']} style={{ marginTop: '0.2rem', marginLeft: '0.3rem' }} xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 30 30" fill="none">
                     <g clip-path="url(#clip0_0_956)">
                         <path d="M18.75 22.5H3.75V20H18.75V22.5ZM26.25 7.5V10H3.75V7.5H26.25ZM18.75 16.25H3.75V13.75H18.75V16.25Z" fill="#1C4980" />
                     </g>
@@ -21,14 +34,13 @@ export function Main() {
                     <path d="M1 0V46" stroke="#DADCE0" />
                 </svg>
                 <span className={style['headerTitle2']}>My Assessments</span>
-
             </header>
             <hr style={{ margin: '0px' }} />
             <div className={style['assessmentOverview']}>
                 <div style={{ marginBottom: "1rem" }}>
                     <span className={style['assessmentTitle']}>Assessments Overview</span>
                 </div>
-                <div className={style['cardDetail']}>
+                <div className={style['cardDetail']} ref = {cardRef}>
                     <div className={style['firstCard']}>
                         <span className={style['TotalAssessment']} >Total Assessment</span>
                         <div className={style['firstContent']}>
@@ -103,7 +115,7 @@ export function Main() {
                             <svg xmlns="http://www.w3.org/2000/svg" width="2" height="40" viewBox="0 0 2 40" fill="none">
                                 <path d="M1 0V40" stroke="rgb(135 139 148)" />
                             </svg>
-                            <div style={{ marginLeft: "2rem", marginRight: '4rem' }}>
+                            <div className = {style['uniqueLink']}>
                                 <span className={style['number']}>145</span>
                                 <span style={{ color: '#05C165', fontFamily: 'Inter', fontSize: '0.75rem', lineHeight: '140%' }}>+89</span>
                                 <span className={style['whoAtemped']}>Unique Link</span>
@@ -130,17 +142,44 @@ export function Main() {
                     </div>
                 </div>
             </div>
+            <div className={style['assessmentSection']}>
+                <div className={style['myassessment']}>
+                    My Assessment
+                </div>
+                <div className={style['assessmentLogo']}>
 
-            <div className={style['myassessment']}>
-                My Assessment
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
+                        <g clip-path="url(#clip0_3566_282)">
+                            <path d="M14.2083 12.8333H13.4842L13.2275 12.5858C14.1258 11.5408 14.6667 10.1842 14.6667 8.70833C14.6667 5.4175 11.9992 2.75 8.70833 2.75C5.4175 2.75 2.75 5.4175 2.75 8.70833C2.75 11.9992 5.4175 14.6667 8.70833 14.6667C10.1842 14.6667 11.5408 14.1258 12.5858 13.2275L12.8333 13.4842V14.2083L17.4167 18.7825L18.7825 17.4167L14.2083 12.8333ZM8.70833 12.8333C6.42583 12.8333 4.58333 10.9908 4.58333 8.70833C4.58333 6.42583 6.42583 4.58333 8.70833 4.58333C10.9908 4.58333 12.8333 6.42583 12.8333 8.70833C12.8333 10.9908 10.9908 12.8333 8.70833 12.8333Z" fill="#1C4980" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_3566_282">
+                                <rect width="22" height="22" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <g clip-path="url(#clip0_3566_286)">
+                            <path d="M4.25 5.66C4.35 5.79 9.99 12.99 9.99 12.99V19C9.99 19.55 10.44 20 11 20H13.01C13.56 20 14.02 19.55 14.02 19V12.98C14.02 12.98 19.51 5.96 19.77 5.64C20.03 5.32 20 5 20 5C20 4.45 19.55 4 18.99 4H5.01C4.4 4 4 4.48 4 5C4 5.2 4.06 5.44 4.25 5.66Z" fill="#1C4980" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_3566_286">
+                                <rect width="24" height="24" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+
+                    <svg onClick={handleOverview} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                        <path d="M0 16V5H4V16H0ZM6 16V0H10V16H6ZM12 16V9H16V16H12Z" fill="#1C4980" />
+                    </svg>
+
+                </div>
             </div>
-
             <div className={style['assessmentCard']}>
                 <Model />
                 <Card />
                 <Card />
-
-
             </div>
         </div>
     )
